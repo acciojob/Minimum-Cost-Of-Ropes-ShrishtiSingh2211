3c50c4804bf5ceefa47bce0940dfdb7d865635c5
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-02-20 17:19:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,55 +2,43 @@ function calculateMinCost() {
   //your code here
 	let inputElement = document.querySelector('#rope-lengths');
 	let inputValue = inputElement.value;
-	let ropeLengths = inputValue.split(',');
 	let ropeLengths = inputValue.split(',').map(Number);
 	class MinHeap {
     constructor() {
         this.heap = [];
     }
-
     getParentIndex(i) {
         return Math.floor((i - 1) / 2);
     }
-
     getLeftChildIndex(i) {
         return 2 * i + 1;
     }
-
     getRightChildIndex(i) {
         return 2 * i + 2;
     }
-
     hasParent(i) {
         return this.getParentIndex(i) >= 0;
     }
-
     hasLeftChild(i) {
         return this.getLeftChildIndex(i) < this.heap.length;
     }
-
     hasRightChild(i) {
         return this.getRightChildIndex(i) < this.heap.length;
     }
-
     parent(i) {
         return this.heap[this.getParentIndex(i)];
     }
-
     leftChild(i) {
         return this.heap[this.getLeftChildIndex(i)];
     }
-
     rightChild(i) {
         return this.heap[this.getRightChildIndex(i)];
     }
-
     swap(i, j) {
         let temp = this.heap[i];
         this.heap[i] = this.heap[j];
         this.heap[j] = temp;
     }
-
     peek() {
         if (this.heap.length == 0) {
             throw "Heap is empty";
@@ -58,7 +46,6 @@ function calculateMinCost() {
             return this.heap[0];
         }
     }
-
     poll() {
         if (this.heap.length == 0) {
             throw "Heap is empty";
@@ -69,7 +56,6 @@ function calculateMinCost() {
             return item;
         }
     }
-
     add(item) {
         this.heap.push(item);
         this.heapifyUp();
@@ -82,7 +68,6 @@ function calculateMinCost() {
             index = this.getParentIndex(index);
         }
     }
-
     heapifyDown() {
         let index = 0;
         while (this.hasLeftChild(index)) {
@@ -96,11 +81,8 @@ function calculateMinCost() {
             } else {
                 this.swap(index, smallerChildIndex);
             }
-
             index = smallerChildIndex;
         }
     }
-}
-   
-  
+}  
 }  
